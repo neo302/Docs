7bdf9bd35039ec5d708813f2aa1961470032be3a
Neo302 <justinleekong@gmail.com>
2019-02-11 12:30:12 -0500

    Revert "Revert "u""
    This reverts commit 16c9817dd745ea5d42dd5344e26305ce5f4139bd.

--- aspnetcore/security/authorization/secure-data/samples/starter2.1/Startup.cs
+++ aspnetcore/security/authorization/secure-data/samples/starter2.1/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.EntityFrameworkCore;
 using ContactManager.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace ContactManager
 {
@@ -37,11 +39,19 @@ namespace ContactManager
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<IdentityUser>().AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(config =>
+            {
+                // using Microsoft.AspNetCore.Mvc.Authorization;
+                // using Microsoft.AspNetCore.Authorization;
+                var policy = new AuthorizationPolicyBuilder()
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
